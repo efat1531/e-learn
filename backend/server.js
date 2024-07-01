ef56ecb888fa2524dfe8458778902e67fcd3494d
Sp3cTer <20103033@iubat.edu>
2024-07-01 17:23:21 +0600

    Added file structure and updated project settings

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
