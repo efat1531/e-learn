be3ce7c796a70b4ee83a03a3c0e1b79d6843c72a
Sp3cTer <20103033@iubat.edu>
2024-07-11 17:40:00 +0600

    Added store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import apiSlice from "../features/api/apiSlice";
+import apiSlice from "../features/api/apiSlice.js";
 
 export const store = configureStore({
   reducer: {
---
+++ frontend/src/features/api/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://localhost:8088";
+export const AUTH_URL = "/api/auth";
+export const USER_URL = "/api/users";
+export const COURSE_URL = "/api/courses";
+export const REVIEW_URL = "/api/reviews";
