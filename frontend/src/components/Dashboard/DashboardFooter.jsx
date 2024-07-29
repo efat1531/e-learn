0301fa61a97ae68add1de61c5833087cf99074a5
shz-code <web.shahidul.alam@gmail.com>
2024-07-29 22:47:27 +0600

    add: Dashboard Footer

---
+++ frontend/src/components/Dashboard/DashboardFooter.jsx
@@ -0,0 +1,13 @@
+const DashboardFooter = () => {
+  return (
+    <footer className="container text-center px-2 py-4">
+      <span className="text-[#8C94A3] text-justify font-inter text-[0.875rem] font-normal leading-[1.375rem] tracking-[-0.00875rem]">
+        &copy; 2024 - <span className="font-[600]">Sonorus</span>.
+        Designed by{" "}
+        <span className="font-[500] text-Primary-500">Efat Sikder</span>. All
+        rights reserved
+      </span>
+    </footer>
+  );
+};
+export default DashboardFooter;
