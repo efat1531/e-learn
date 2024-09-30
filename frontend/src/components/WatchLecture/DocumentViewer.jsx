15566fe6e8f82cc9f4ae1e508f643314e7bb3a2b
shz-code <web.shahidul.alam@gmail.com>
2024-09-30 15:09:53 +0600

    add: Documet Viewer

--- frontend/src/components/WatchLecture/DocumentViewer.jsx
+++ frontend/src/components/WatchLecture/DocumentViewer.jsx
@@ -1,6 +1,54 @@
-const DocumentViewer = () => {
+import PropTypes from "prop-types";
+import VideoPlayer from "../ui/VideoPlayer";
+import { durationConversion } from "../../utils/Transformations";
+import Button from "../ui/Button";
+import { Link, useParams } from "react-router-dom";
+
+const DocumentViewer = ({ currentLecture, previousLecture, nextLecture }) => {
+  const { slug } = useParams();
+
+  const { contentURL, contentDescription, contentDuration, contentTitle } =
+    currentLecture;
+
+  console.log(currentLecture);
+
   return (
-    <div>DocumentViewer</div>
-  )
-}
-export default DocumentViewer
+    <div>
+      <div className="border p-4">
+        <div className="flex justify-between text-CustomGray-900 font-[500] leading-6">
+          <h4>{contentTitle}</h4>
+        </div>
+        <div className="p-4 bg-gray-50">
+          <a href={contentURL} className="underline" target="_blank">Download Document</a>
+        </div>
+        <p className="mt-4">{contentDescription}</p>
+        <div className="flex justify-between gap-4 mt-4">
+          <Link
+            to={
+              previousLecture != null &&
+              `/courses/${slug}/lecture/${previousLecture._id}`
+            }
+          >
+            <Button title="Prev" disabled={previousLecture === null} />
+          </Link>
+          <Button secondary title="Mart as complete" />
+          <Link
+            to={
+              nextLecture != null &&
+              `/courses/${slug}/lecture/${nextLecture._id}`
+            }
+          >
+            <Button title="Next" disabled={nextLecture === null} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default DocumentViewer;
+
+DocumentViewer.propTypes = {
+  currentLecture: PropTypes.object.isRequired,
+  previousLecture: PropTypes.object,
+  nextLecture: PropTypes.object,
+};
